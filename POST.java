640c4dc10c311287b41880960404d2208506a3d3
2023320006정준영 <jjyoung0522@naver.com>
2026-06-05 16:21:36 +0900

    주석 추가

--- POST.java
+++ POST.java
@@ -1,5 +1,4 @@
 
-
 /**
  * POST 클래스의 설명을 작성하세요.
  *
@@ -12,30 +11,66 @@ public class POST
     private Products[] productDB;
     private int saleCount;
     private int productCount;
-    
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
     public POST() {
         this.saleDB = new Sale[1000];
         this.productDB = new Products[100];
         this.productCount = 0;
         this.saleCount = 0;
     }
-    
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
     public int priceCalculate() {
-        
+
     }
-    
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
     public String printProducts() {
-        
+
     }
-    
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
     public int changeCalculate() {
-        
+
     }
-    
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
     public Sale saleInfoSave() {
-    
+
     }
-    
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
     public Products setProduct(Products product) {
         for (int i = 0; i < productCount; i++) {
             if (productDB[i].getBarcodeNumber() == product.getBarcodeNumber()) {
