d71b06d7d44af1e32919bc8a4ef75089d0f11788
2023320006정준영 <jjyoung0522@naver.com>
2026-06-05 11:23:16 +0900

    Product 작성

---
+++ Products.java
@@ -0,0 +1,13 @@
+
+/**
+ * Product 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public abstract class Products
+{   
+    private String productName;
+    private int productPrice;
+    private Long barcodeNumber;
+}
