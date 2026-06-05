36a9fd80429c11c6f85563edfbff83665aef70fe
Kangseongha <ddukkukang1@gmail.com>
2026-06-05 11:54:43 +0900

    ㅇ

--- AlcoholicDrinks.java
+++ AlcoholicDrinks.java
@@ -6,10 +6,16 @@
  * @version (버전 번호 또는 작성한 날짜)
  */
 public class AlcoholicDrinks extends Product implements Tax{ 
-    private double taxLate; 
-    private double alcohoilcTax;
+    private double taxLate;  
+    private double alcohoilcTax; 
+    public AlcoholicDrinks(String productName, int productPrice, long barcodeNumber,
+    double taxLate, double alcoholicTax){
+    super(productName, productPrice, barcodeNumber); 
+    this.taxLate = taxLate;
+    this.alcoholicTax = alcoholicTax;} 
+    
     public int calculateTax(){ 
-    } 
+    }//세금 계산하는 메소드 
     public int getTotalPrice(){ 
-    } 
+    }//술의 총 가격을 알려주는 메소드
 }
