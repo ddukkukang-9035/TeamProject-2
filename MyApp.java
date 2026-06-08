5c4c03aacf382b351b992ae3e51303b0a546f51b
psj5317 <tjdwns6887@naver.com>
2026-06-09 00:22:48 +0900

    myapp 수정

--- MyApp.java
+++ MyApp.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * MyApp 클래스의 설명을 작성하세요.
  *
@@ -8,7 +8,21 @@
 public class MyApp
 {
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
         POST post = new POST();
         
+        System.out.println("========================================");
+        System.out.println("   POST System - 편의점 계산 시스템");
+        System.out.println("========================================");
+        
+        boolean running = true;
+        while (running) {
+            post.resetSale();
+            System.out.println("\n[새 고객] 상품을 스캔하세요.");
+            System.out.println(post.printProducts());
+            
+            
     }
+    
+    
 }
