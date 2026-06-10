2296f04edc2a380e66e62097fd23639face337ae
psj5317 <tjdwns6887@naver.com>
2026-06-10 22:16:54 +0900

    Beverages

--- Beverages.java
+++ Beverages.java
@@ -13,4 +13,10 @@ public class Beverages extends Products{
         super(barcode, name, price, quantity);
         this.category = category;
     }
+    
+    public static void initDB() {
+        addToDB(new Beverages("B001", "코카콜라", "탄산음료", 1500, 1));
+        addToDB(new Beverages("B002", "포카리스웨트", "이온음료", 1800, 1));
+        addToDB(new Beverages("B003", "제주삼다수", "생수",900, 1));
+    }
 }
