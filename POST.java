31450a9e4c7febc67e072e0b9d5aa371c6ebe41c
Kangseongha <ddukkukang1@gmail.com>
2026-06-11 15:43:47 +0900

    Post수정

--- POST.java
+++ POST.java
@@ -1,83 +1,52 @@
+import java.util.Scanner;
 
 /**
- * POST 클래스의 설명을 작성하세요.
+ * POST Class  (Point Of Sale Terminal)
+ * 비즈니스 로직 전담 클래스.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * 상품DB는 Products가, 판매DB는 Sale이 각자 관리한다.
+ * POST는 두 클래스의 static 메소드를 호출하여 판매 흐름을 조율한다.
  */
-public class POST
-{
-    private Sale[] saleDB;
-    private Products[] productDB;
-    private int saleCount;
-    private int productCount;
+public class POST {
+    private Sale    currentSale;
+    private Scanner sc = new Scanner(System.in);
 
-    /**    ㅠㅍㅊ
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
+    /**
+     * POST 생성 시 상품DB를 초기화한다.
+     * (상품DB 데이터는 Products 클래스가 소유)
      */
     public POST() {
-        this.saleDB = new Sale[1000];
-        this.productDB = new Products[100];
-        this.productCount = 0;
-        this.saleCount = 0;
+        AlcoholicDrinks.initDB();
+        Beverages.initDB();
     }
 
     /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
+     * 바코드 입력 → Products.findByBarcode() 조회 ,currentSale에 추가.
+     * UCD Line 2~3 반복 구간.
+     * @return 계속 스캔해야 하면 true, 'done' 입력이면 false
      */
-    public int priceCalculate() {
+    public boolean scanProduct() {
+        System.out.print("바코드 입력 (완료: '0'): ");
+        String barcode = sc.next();
 
-    }
-
-    /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
-     */
-    public String printProducts() {
-
-    }
-
-    /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
-     */
-    public int changeCalculate() {
-
-    }
-
-    /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
-     */
-    public Sale saleInfoSave() {
-
-    }
-
-    /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
-     */
-    public Products setProduct(Products product) {
-        for (int i = 0; i < productCount; i++) {
-            if (productDB[i].getBarcodeNumber() == product.getBarcodeNumber()) {
-                return productDB[i];
-            }
+        if (barcode.equalsIgnoreCase("0")) {
+            return false;
         }
-        productDB[productCount++] = product;
-        return product;
+        
+        Products found = Products.findByBarcode(barcode);
+        if (found == null) {
+            System.out.println("  [오류] 올바르지 않은 바코드입니다: " + barcode);
+            return true;
+        }
+
+        System.out.print("  수량 입력: ");
+        int qty = sc.nextInt();
+
+        Products item = cloneWithQuantity(found, qty);
+
+        currentSale.addProduct(item);
+
+        System.out.println("  [추가됨] " + item.getName() + " × " + qty + "  →  소계: " + item.calcAmount() + "원");
+
+        return true;
     }
-}
